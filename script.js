acf4384804854ae31f099c6af8f165d5dbb7ab03
yadnyavalkya dakhore <yadnyavalkyakd.a04@gmail.com>
2025-02-22 13:44:43 +0530

    updated ui

--- script.js
+++ script.js
@@ -55,3 +55,20 @@ function addCountdownStars() {
 
 createBackgroundStars();
 updateCountdown();
+
+function startCountdown() {
+    const eventDate = new Date('March 5, 2025 11:00:00').getTime();
+    setInterval(() => {
+        const now = new Date().getTime();
+        const timeLeft = eventDate - now;
+        
+        const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+        
+        document.getElementById('countdown').innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    }, 1000);
+}
+
+startCountdown();
